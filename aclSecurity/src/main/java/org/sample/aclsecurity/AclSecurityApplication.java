10ea23eb9d493354d8aef61559434f98260eac8a
nedaakbari <nedaakbari555@gmail.com>
2024-02-29 22:58:28 +0330

    update

---
+++ aclSecurity/src/main/java/org/sample/aclsecurity/AclSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.sample.aclsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AclSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AclSecurityApplication.class, args);
+	}
+
+}
---
+++ aclSecurity/src/test/java/org/sample/aclsecurity/AclSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sample.aclsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AclSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
